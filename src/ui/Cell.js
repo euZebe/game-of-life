ab2340a85d756e376c2d76ef938895fed54b0eb4
euzebe <euzebe@axiome.me>
2018-03-30 11:23:32 +0200

    refresh optimisation

--- src/ui/Cell.js
+++ src/ui/Cell.js
@@ -3,19 +3,26 @@ import PropTypes from 'prop-types';
 import { ALIVE, DEAD } from '../store/cell-duck';
 import './App.css';
 
-const Cell = ({status, toggleState}) => {
-  return status === ALIVE
-    ? <span className="cell" role="img" aria-label="alive" onClick={toggleState}>⬛</span>
-    : <span className="cell" role="img" aria-label="dead" onClick={toggleState}>⬜</span>;
-};
+class Cell extends React.Component {
+
+  shouldComponentUpdate(nextProps) {
+    return nextProps.status !== this.props.status;
+  }
+
+  render() {
+    const {status, toggleState} = this.props;
+    return status === ALIVE
+      ? <span className="cell" role="img" aria-label="alive" onClick={toggleState}>⬛</span>
+      : <span className="cell" role="img" aria-label="dead" onClick={toggleState}>⬜</span>;
+  }
+}
 
 Cell.propTypes = {
-  state: PropTypes.string,
+  status: PropTypes.string.isRequired,
   toggleState: PropTypes.func,
 };
 
 Cell.defaultProps = {
-  state: DEAD,
   toggleState: () => {},
 };
 
