63e5f47a7071ad27a007c612a0629ef473b68927
euzebe <euzebe@axiome.me>
2018-04-02 11:06:12 +0200

    enable toggling cells when ctrlKey + mouseEnter event

--- src/ui/Cell.js
+++ src/ui/Cell.js
@@ -3,17 +3,32 @@ import PropTypes from 'prop-types';
 import { ALIVE } from '../store/cell-duck';
 import './App.css';
 
+const DefaultCell = ({ label, ...rest }) => (
+  <span
+    className="cell"
+    role="img"
+    {...rest}
+ >
+    {label}
+  </span>
+);
+
 class Cell extends React.Component {
 
   shouldComponentUpdate(nextProps) {
     return nextProps.status !== this.props.status;
   }
 
+  handleMouseEnter = (e) => {
+    e.ctrlKey && this.props.toggleStatus();
+  }
+
   render() {
     const {status, toggleStatus} = this.props;
     return status === ALIVE
-      ? <span className="cell" role="img" aria-label="alive" onClick={toggleStatus}>⬛</span>
-      : <span className="cell" role="img" aria-label="dead" onClick={toggleStatus}>⬜</span>;
+      ? <DefaultCell aria-label="alive" onClick={toggleStatus} onMouseEnter={this.handleMouseEnter} label="⬛" />
+      : <DefaultCell aria-label="dead" onClick={toggleStatus} onMouseEnter={this.handleMouseEnter} label="⬜" />
+    ;
   }
 }
 
